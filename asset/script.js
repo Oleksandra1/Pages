7c7e5e4bee5bf7c6f2a0ce2277c117a3f4e1d485
Oleksandra1 <oleksandra.tkachenko154.com>
2024-08-03 14:21:38 +0300

    home_page

--- asset/script.js
+++ asset/script.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('#top').fadeIn();
+        } else {
+            $('#top').fadeOut();
+        }        
+    });
+
+    $('#top').click(function(){
+        $('html,body').animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+
     $(".owl-carousel-full").owlCarousel({
         margin: 20,
         responsive: {
